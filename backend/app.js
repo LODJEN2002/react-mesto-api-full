647ecff8b2dd3d963f479fcc9907c984b5621deb
Ivan Docenko <decel1337@gmail.com>
2022-11-23 14:36:53 +0300

    react-mesto-api-full

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const { errors } = require('celebrate');
 const router = require('./routes/index');
 const centralErorr = require('./errors/centralError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-require('dotenv').config();
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
